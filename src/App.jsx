b822fa87a187bcf473dbfe06e429e310245b3301
federicoacker <acker.federico@gmail.com>
2026-05-12 13:57:01 +0200

    Inizio a lavorare al Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router";
+import PageNotFound from "./components/pages/PageNotFound";
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <Route index/>
       <Route path="about-us"/>
       <Route path="products"/>
+      <Route path="*" Component={PageNotFound}/>
     </Routes>
   );
 }
