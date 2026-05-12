0c3e0af0be6853e92d13239436566dba02b62364
federicoacker <acker.federico@gmail.com>
2026-05-12 14:35:36 +0200

    Aggiunto un pochino di stile

--- src/components/layout/PageLayout.jsx
+++ src/components/layout/PageLayout.jsx
@@ -6,16 +6,16 @@ import Ad from "../Ad.jsx";
 function PageLayout() {
     return (
         <>
-            <div className="d-flex flex-column min-vh-100">
+            <div className="d-flex flex-column min-vh-100 bg-dark">
                 <Header />
                 <div className="d-flex flex-grow-1">
-                    <div className="d-flex align-items-center ad-wrapper px-2">
+                    <div className="d-flex align-items-center ad-wrapper px-2 border rounded border-white">
                         <Ad starterIndex={1}/>
                     </div>
-                    <main className="flex-grow-1">
+                    <main className="flex-grow-1 px-3">
                         <Outlet/>
                     </main>
-                    <div className="d-flex align-items-center ad-wrapper px-2">
+                    <div className="d-flex align-items-center ad-wrapper px-2 border rounded border-white">
                         <Ad starterIndex={0}/>
                     </div>
                 </div>
