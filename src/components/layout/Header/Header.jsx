6739b75ca315f3b41206c95461a1872d801a56df
federicoacker <acker.federico@gmail.com>
2026-05-12 14:43:24 +0200

    Cominciato a lavorare all'header

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,7 +1,31 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link } from "react-router";
 
 function Header() {
     return (
-        <div>Header</div>
+        <header>
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand >
+                        <Link to="/" className="text-decoration-none">Logo</Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="site-navbar" />
+                    <Navbar.Collapse id="site-navbar">
+                        <Nav className="me-auto">
+                            <Nav.Link >
+                                <Link to="/" className="text-decoration-none">Home</Link>
+                            </Nav.Link>
+                            <Nav.Link>
+                                <Link to="about-us" className="text-decoration-none">Chi Siamo?</Link>
+                            </Nav.Link>
+                            <Nav.Link>
+                                <Link to="products" className="text-decoration-none">I Nostri Prodotti</Link>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
     )
 }
 
