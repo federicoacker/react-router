4e955dac7bb8308b1fa3280648f30efa6f30f172
federicoacker <acker.federico@gmail.com>
2026-05-12 17:21:27 +0200

    Altra grande miglioria alla pagina 404

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,7 +1,7 @@
 
 function PageNotFound() {
   return (
-    <div className="d-flex flex-column justify-content-between align-items-center">
+    <div className="d-flex flex-column justify-content-center align-items-center h-100">
       <h1>404 - La Pagina Che Stai Cercando Si E' Persa O Forse Sei Tu Ad Essere Un Coglione E Aver Digitato L'indirizzo Sbagliato</h1>
     </div>
   )
